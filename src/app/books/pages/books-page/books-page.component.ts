2ff5805f9136b678da97c1c943fd232216488d53
David Pardo <dajupaco@gmail.com>
2025-10-06 22:29:31 +0200

    feat(book-table): finished styling

--- src/app/books/pages/books-page/books-page.component.ts
+++ src/app/books/pages/books-page/books-page.component.ts
@@ -1,21 +1,51 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, Input } from '@angular/core';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { AfterViewInit, ChangeDetectionStrategy, Component, inject, Input, ViewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Router, RouterModule } from '@angular/router';
 import { BookDTO, BookstoreBffService } from '@openapi';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'mxs-books-page',
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatDividerModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatIconModule,
+    CurrencyPipe,
+  ],
   templateUrl: './books-page.component.html',
   styleUrl: './books-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BooksPageComponent {
-  public readonly books$: Observable<BookDTO[]> = inject(BookstoreBffService).getBooks({ onSale: false });
+export class BooksPageComponent implements AfterViewInit {
+  protected readonly bookstoreBffService = inject(BookstoreBffService);
   protected readonly router = inject(Router);
 
+  protected dataSource: MatTableDataSource<BookDTO> = new MatTableDataSource<BookDTO>([]);
+
   @Input() bookId = '';
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  readonly displayedColumns: string[] = ['title', 'price', 'onSale', 'edit', 'delete'];
+
+  ngAfterViewInit() {
+    this.bookstoreBffService.getBooks({ onSale: false }).subscribe(books => {
+      this.dataSource = new MatTableDataSource<BookDTO>(books);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
 
   createBook() {
     this.router.navigate(['/books/new']);
@@ -23,5 +53,9 @@ export class BooksPageComponent {
 
   editBook(bookId: string) {
     this.router.navigate([`/books/edit/${bookId}`]);
+  }
+
+  deleteBook(bookId: string) {
+    //to implement
   }
 }
