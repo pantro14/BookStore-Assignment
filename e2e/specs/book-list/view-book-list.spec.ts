d81f23eeabcad414b7588e352b4bec2b8c933ff7
David Pardo <dajupaco@gmail.com>
2025-10-09 21:33:54 +0200

    test(e2e): refactoring mock api interceptor into a helper

--- e2e/specs/book-list/view-book-list.spec.ts
+++ e2e/specs/book-list/view-book-list.spec.ts
@@ -1,16 +1,11 @@
 import { expect, test } from '@playwright/test';
+import { mockApiResponse } from 'e2e/helpers/mock-api-response';
 
-import data from '../../mocks/api-data/book/books_get_200_data.json';
+import data from '../../../mocks/api-data/book/books_get_200_data.json';
 
 test.describe('Use Case 1: viewing all books ', () => {
   test.beforeEach(async ({ page }) => {
-    await page.route('**/book-store-bff/v1/books?onSale=false', async route => {
-      await route.fulfill({
-        status: 200,
-        contentType: 'application/json',
-        body: JSON.stringify(data),
-      });
-    });
+    await mockApiResponse(page, '**/book-store-bff/v1/books?onSale=false', data);
   });
 
   test('should show the book table', async ({ page }) => {
--- e2e/specs/book-list/view-empty-book-list.spec.ts
+++ e2e/specs/book-list/view-empty-book-list.spec.ts
@@ -1,16 +1,11 @@
 import { expect, test } from '@playwright/test';
+import { mockApiResponse } from 'e2e/helpers/mock-api-response';
 
-import data from '../../mocks/api-data/book/books_get_200_empty-list_data.json';
+import data from '../../../mocks/api-data/book/books_get_200_empty-list_data.json';
 
 test.describe('Use Case 1: viewing all books ', () => {
   test.beforeEach(async ({ page }) => {
-    await page.route('**/book-store-bff/v1/books?onSale=false', async route => {
-      await route.fulfill({
-        status: 200,
-        contentType: 'application/json',
-        body: JSON.stringify(data),
-      });
-    });
+    await mockApiResponse(page, '**/book-store-bff/v1/books?onSale=false', data);
   });
 
   test('should display: title, price, sale status and action buttons.', async ({ page }) => {
