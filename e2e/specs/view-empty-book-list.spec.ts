8183c0a818fc7d35147a495fbea624b00c220c4f
David Pardo <dajupaco@gmail.com>
2025-10-09 21:25:23 +0200

    test(e2e): finished basic setup for tests

---
+++ e2e/specs/view-empty-book-list.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from '@playwright/test';
+
+import data from '../../mocks/api-data/book/books_get_200_empty-list_data.json';
+
+test.describe('Use Case 1: viewing all books ', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.route('**/book-store-bff/v1/books?onSale=false', async route => {
+      await route.fulfill({
+        status: 200,
+        contentType: 'application/json',
+        body: JSON.stringify(data),
+      });
+    });
+  });
+
+  test('should display: title, price, sale status and action buttons.', async ({ page }) => {
+    await page.goto('/');
+
+    const bookItems = await page.getByTestId('book-item');
+
+    await expect(bookItems).toHaveCount(0);
+  });
+});
